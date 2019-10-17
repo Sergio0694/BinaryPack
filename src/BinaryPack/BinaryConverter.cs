4021577b913a3ac9bfd8bf53b8832122714e0675
Sergio Pedri <sergio0694@live.com>
2019-10-17 16:13:06 +0200

    Added draft version of deserializer

--- src/BinaryPack/BinaryConverter.cs
+++ src/BinaryPack/BinaryConverter.cs
@@ -4,8 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using BinaryPack.Extensions;
 
 namespace BinaryPack
@@ -44,6 +42,44 @@ namespace BinaryPack
 
                 il.Emit(OpCodes.Ret);
             })(obj, stream);
+        }
+
+        public static T Deserialize<T>(Stream stream) where T : new()
+        {
+            return DynamicMethod<BinaryDeserializer<T>>.New(il =>
+            {
+                IEnumerable<PropertyInfo> properties =
+                    from prop in typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                    where prop.CanRead && prop.CanWrite
+                    select prop;
+
+                il.DeclareLocal(typeof(T));
+                il.DeclareLocal(typeof(Span<byte>));
+
+                il.Emit(OpCodes.Newobj, KnownMethods.Type<T>.DefaultConstructor);
+                il.EmitStoreLocal(0);
+
+                foreach (PropertyInfo property in properties)
+                {
+                    il.EmitStackalloc(property.PropertyType);
+                    il.EmitLoadInt32(property.PropertyType.GetSize());
+                    il.Emit(OpCodes.Newobj, KnownMethods.Span<byte>.UnsafeConstructor);
+                    il.EmitStoreLocal(1);
+                    il.Emit(OpCodes.Ldarg_0);
+                    il.EmitLoadLocal(1);
+                    il.EmitCall(OpCodes.Callvirt, KnownMethods.Stream.Read, null);
+                    il.Emit(OpCodes.Pop);
+                    il.EmitLoadLocal(0);
+                    il.Emit(OpCodes.Ldloca_S, 1);
+                    il.EmitCall(OpCodes.Call, KnownMethods.Span<byte>.GetPinnableReference, null);
+                    il.EmitCall(OpCodes.Call, KnownMethods.Unsafe<byte>.As(property.PropertyType), null);
+                    il.EmitLoadFromAddress(property.PropertyType);
+                    il.EmitWriteMember(property);
+                }
+
+                il.EmitLoadLocal(0);
+                il.Emit(OpCodes.Ret);
+            })(stream);
         }
     }
 }
