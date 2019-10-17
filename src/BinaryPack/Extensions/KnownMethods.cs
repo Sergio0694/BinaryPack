c8f25a5e98bad75b6c9b97f29048e48866ff4369
Sergio Pedri <sergio0694@live.com>
2019-10-17 14:53:15 +0200

    Added more known methods, code tweaks

--- src/BinaryPack/Extensions/KnownMethods.cs
+++ src/BinaryPack/Extensions/KnownMethods.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
@@ -33,21 +35,68 @@ namespace BinaryPack.Extensions
         }
 
         /// <summary>
+        /// A <see langword="class"/> containing methods from the <see cref="System.ReadOnlySpan{T}"/> type
+        /// </summary>
+        /// <typeparam name="T">The generic type argument for the target <see cref="System.ReadOnlySpan{T}"/> type</typeparam>
+        public static class ReadOnlySpan<T>
+        {
+            /// <summary>
+            /// Gets the <see cref="System.ReadOnlySpan{T}"/> constructor that takes a <see langword="void"/> pointer and a size
+            /// </summary>
+            public static ConstructorInfo UnsafeConstructor { get; } = (
+                from ctor in typeof(System.Span<T>).GetConstructors()
+                let args = ctor.GetParameters()
+                where args.Length == 2 &&
+                      args[0].ParameterType == typeof(void*) &&
+                      args[1].ParameterType == typeof(int)
+                select ctor).First();
+        }
+
+        /// <summary>
         /// A <see langword="class"/> containing methods from the <see cref="Unsafe"/> type
         /// </summary>
-        /// <typeparam name="TFrom">The first type argument for generic methods</typeparam>
-        /// <typeparam name="TTo">The second type argument for generic methods</typeparam>
-        public static class Unsafe<TFrom, TTo>
+        /// <typeparam name="T">The generic type argument for generic methods</typeparam>
+        public static class Unsafe<T>
         {
+            // Mapping of methods for target type not known at compile time
+            private static readonly Dictionary<Type, MethodInfo> _AsMap = new Dictionary<Type, MethodInfo>();
+
             /// <summary>
             /// Gets the <see cref="MethodInfo"/> instance mapping the <see cref="Unsafe.As{TFrom,TTo}"/> method
             /// </summary>
-            public static MethodInfo As { get; } = (
-                from method in typeof(Unsafe).GetMethods()
-                where method.Name.Equals(nameof(Unsafe.As)) &&
-                      method.GetGenericArguments().Length == 2
-                let target = method.MakeGenericMethod(typeof(TFrom), typeof(TTo))
-                select target).First();
+            public static MethodInfo As(Type type)
+            {
+                if (!_AsMap.TryGetValue(type, out MethodInfo methodInfo))
+                {
+                    methodInfo = (
+                        from method in typeof(Unsafe).GetMethods()
+                        where method.Name.Equals(nameof(Unsafe.As)) &&
+                              method.GetGenericArguments().Length == 2
+                        let target = method.MakeGenericMethod(typeof(T), type)
+                        select target).First();
+                    _AsMap.Add(type, methodInfo);
+                }
+
+                return methodInfo;
+            }
+        }
+
+        /// <summary>
+        /// A <see langword="class"/> containing methods from the <see cref="Unsafe"/> type
+        /// </summary>
+        /// <typeparam name="T">The generic type argument for generic methods</typeparam>
+        public static class Stream
+        {
+            /// <summary>
+            /// Gets the <see cref="MethodInfo"/> instance mapping the <see cref="System.IO.Stream.Write(System.ReadOnlySpan{byte})"/> method
+            /// </summary>
+            public static MethodInfo Write { get; } = (
+                from method in typeof(System.IO.Stream).GetMethods(BindingFlags.Public | BindingFlags.Instance)
+                where method.Name.Equals(nameof(System.IO.Stream.Write))
+                let args = method.GetParameters()
+                where args.Length == 1 &&
+                      args[0].ParameterType == typeof(ReadOnlySpan<byte>)
+                select method).First();
         }
     }
 }
