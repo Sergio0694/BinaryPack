e9dcc0cedce968d66ec5fa05a34703dcb1aed991
Sergio Pedri <sergio0694@live.com>
2019-10-17 17:02:06 +0200

    Code refactoring

--- src/BinaryPack/BinaryConverter.cs
+++ src/BinaryPack/BinaryConverter.cs
@@ -1,56 +1,48 @@
 ﻿using System;
 using System.Buffers;
-using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Reflection.Emit;
-using BinaryPack.Delegates;
-using BinaryPack.Extensions;
+using BinaryPack.Serialization;
 
 namespace BinaryPack
 {
+    /// <summary>
+    /// The entry point <see langword="class"/> for all the APIs in the library
+    /// </summary>
     public static class BinaryConverter
     {
+        /// <summary>
+        /// Serializes the input <typeparamref name="T"/> instance and returns a <see cref="Memory{T}"/> instance
+        /// </summary>
+        /// <typeparam name="T">The type of instance to serialize</typeparam>
+        /// <param name="obj">The input instance to serialize</param>
+        /// <returns>A <see cref="Memory{T}"/> instance containing the serialized data</returns>
         public static Memory<byte> Serialize<T>(T obj) where T : new()
         {
             using MemoryStream stream = new MemoryStream();
             Serialize(obj, stream);
-
             byte[] data = stream.GetBuffer();
+
             return new Memory<byte>(data, 0, (int)stream.Position);
         }
 
+        /// <summary>
+        /// Serializes the input <typeparamref name="T"/> instance to the target <see cref="Stream"/>
+        /// </summary>
+        /// <typeparam name="T">The type of instance to serialize</typeparam>
+        /// <param name="obj">The input instance to serialize</param>
+        /// <param name="stream">The <see cref="Stream"/> instance to use to write the data</param>
         public static void Serialize<T>(T obj, Stream stream) where T : new()
         {
-            DynamicMethod<BinarySerializer<T>>.New(il =>
-            {
-                IEnumerable<PropertyInfo> properties =
-                    from prop in typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance)
-                    where prop.CanRead && prop.CanWrite
-                    select prop;
-
-                il.DeclareLocal(typeof(byte*));
-
-                foreach (PropertyInfo property in properties)
-                {
-                    il.EmitStackalloc(property.PropertyType);
-                    il.EmitStoreLocal(0);
-                    il.EmitLoadLocal(0);
-                    il.Emit(OpCodes.Ldarg_0);
-                    il.EmitReadMember(property);
-                    il.EmitStoreToAddress(property.PropertyType);
-                    il.Emit(OpCodes.Ldarg_1);
-                    il.EmitLoadLocal(0);
-                    il.EmitLoadInt32(property.PropertyType.GetSize());
-                    il.Emit(OpCodes.Newobj, KnownMethods.ReadOnlySpan<byte>.UnsafeConstructor);
-                    il.EmitCall(OpCodes.Callvirt, KnownMethods.Stream.Write, null);
-                }
-
-                il.Emit(OpCodes.Ret);
-            })(obj, stream);
+            SerializationProcessor<T>.Serializer(obj, stream);
         }
 
+        /// <summary>
+        /// Deserializes a <typeparamref name="T"/> instance from the input <see cref="Memory{T}"/> instance
+        /// </summary>
+        /// <typeparam name="T">The type of instance to deserialize</typeparam>
+        /// <param name="memory">The input <see cref="Memory{T}"/> instance to read data from</param>
+        [Pure]
         public static unsafe T Deserialize<T>(Memory<byte> memory) where T : new()
         {
             using MemoryHandle handle = memory.Pin();
@@ -59,41 +51,15 @@ namespace BinaryPack
             return Deserialize<T>(stream);
         }
 
+        /// <summary>
+        /// Deserializes a <typeparamref name="T"/> instance from the input <see cref="Stream"/> instance
+        /// </summary>
+        /// <typeparam name="T">The type of instance to deserialize</typeparam>
+        /// <param name="stream">The input <see cref="Stream"/> instance to read data from</param>
+        [Pure]
         public static T Deserialize<T>(Stream stream) where T : new()
         {
-            return DynamicMethod<BinaryDeserializer<T>>.New(il =>
-            {
-                IEnumerable<PropertyInfo> properties =
-                    from prop in typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance)
-                    where prop.CanRead && prop.CanWrite
-                    select prop;
-
-                il.DeclareLocal(typeof(T));
-                il.DeclareLocal(typeof(Span<byte>));
-
-                il.Emit(OpCodes.Newobj, KnownMethods.Type<T>.DefaultConstructor);
-                il.EmitStoreLocal(0);
-
-                foreach (PropertyInfo property in properties)
-                {
-                    il.EmitStackalloc(property.PropertyType);
-                    il.EmitLoadInt32(property.PropertyType.GetSize());
-                    il.Emit(OpCodes.Newobj, KnownMethods.Span<byte>.UnsafeConstructor);
-                    il.EmitStoreLocal(1);
-                    il.Emit(OpCodes.Ldarg_0);
-                    il.EmitLoadLocal(1);
-                    il.EmitCall(OpCodes.Callvirt, KnownMethods.Stream.Read, null);
-                    il.Emit(OpCodes.Pop);
-                    il.EmitLoadLocal(0);
-                    il.Emit(OpCodes.Ldloca_S, 1);
-                    il.EmitCall(OpCodes.Call, KnownMethods.Span<byte>.GetPinnableReference, null);
-                    il.EmitLoadFromAddress(property.PropertyType);
-                    il.EmitWriteMember(property);
-                }
-
-                il.EmitLoadLocal(0);
-                il.Emit(OpCodes.Ret);
-            })(stream);
+            return SerializationProcessor<T>.Deserializer(stream);
         }
     }
 }
---
+++ src/BinaryPack/Serialization/SerializationProcessor{T}.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using BinaryPack.Delegates;
+using BinaryPack.Extensions;
+
+namespace BinaryPack.Serialization
+{
+    /// <summary>
+    /// A <see langword="class"/> responsible for creating the serializers and deserializers
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    internal static class SerializationProcessor<T> where T : new()
+    {
+        /// <summary>
+        /// Gets the <see cref="BinarySerializer{T}"/> instance for the current type <typeparamref name="T"/>
+        /// </summary>
+        public static BinarySerializer<T> Serializer { get; } = BuildSerializer();
+
+        /// <summary>
+        /// Gets the <see cref="BinaryDeserializer{T}"/> instance for the current type <typeparamref name="T"/>
+        /// </summary>
+        public static BinaryDeserializer<T> Deserializer { get; } = BuildDeserializer();
+
+        /// <summary>
+        /// Builds a new <see cref="BinarySerializer{T}"/> instance for the type <typeparamref name="T"/>
+        /// </summary>
+        [Pure]
+        private static BinarySerializer<T> BuildSerializer()
+        {
+            return DynamicMethod<BinarySerializer<T>>.New(il =>
+            {
+                IEnumerable<PropertyInfo> properties =
+                    from prop in typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                    where prop.CanRead && prop.CanWrite
+                    select prop;
+
+                il.DeclareLocal(typeof(byte*));
+
+                foreach (PropertyInfo property in properties)
+                {
+                    il.EmitStackalloc(property.PropertyType);
+                    il.EmitStoreLocal(0);
+                    il.EmitLoadLocal(0);
+                    il.Emit(OpCodes.Ldarg_0);
+                    il.EmitReadMember(property);
+                    il.EmitStoreToAddress(property.PropertyType);
+                    il.Emit(OpCodes.Ldarg_1);
+                    il.EmitLoadLocal(0);
+                    il.EmitLoadInt32(property.PropertyType.GetSize());
+                    il.Emit(OpCodes.Newobj, KnownMethods.ReadOnlySpan<byte>.UnsafeConstructor);
+                    il.EmitCall(OpCodes.Callvirt, KnownMethods.Stream.Write, null);
+                }
+
+                il.Emit(OpCodes.Ret);
+            });
+        }
+
+        /// <summary>
+        /// Builds a new <see cref="BinaryDeserializer{T}"/> instance for the type <typeparamref name="T"/>
+        /// </summary>
+        /// <returns></returns>
+        [Pure]
+        private static BinaryDeserializer<T> BuildDeserializer()
+        {
+            return DynamicMethod<BinaryDeserializer<T>>.New(il =>
+            {
+                IEnumerable<PropertyInfo> properties =
+                    from prop in typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                    where prop.CanRead && prop.CanWrite
+                    select prop;
+
+                il.DeclareLocal(typeof(T));
+                il.DeclareLocal(typeof(Span<byte>));
+
+                il.Emit(OpCodes.Newobj, KnownMethods.Type<T>.DefaultConstructor);
+                il.EmitStoreLocal(0);
+
+                foreach (PropertyInfo property in properties)
+                {
+                    il.EmitStackalloc(property.PropertyType);
+                    il.EmitLoadInt32(property.PropertyType.GetSize());
+                    il.Emit(OpCodes.Newobj, KnownMethods.Span<byte>.UnsafeConstructor);
+                    il.EmitStoreLocal(1);
+                    il.Emit(OpCodes.Ldarg_0);
+                    il.EmitLoadLocal(1);
+                    il.EmitCall(OpCodes.Callvirt, KnownMethods.Stream.Read, null);
+                    il.Emit(OpCodes.Pop);
+                    il.EmitLoadLocal(0);
+                    il.Emit(OpCodes.Ldloca_S, 1);
+                    il.EmitCall(OpCodes.Call, KnownMethods.Span<byte>.GetPinnableReference, null);
+                    il.EmitLoadFromAddress(property.PropertyType);
+                    il.EmitWriteMember(property);
+                }
+
+                il.EmitLoadLocal(0);
+                il.Emit(OpCodes.Ret);
+            });
+        }
+    }
+}
