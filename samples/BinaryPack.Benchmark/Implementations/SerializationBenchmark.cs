a5da0a0b3f0b66d50d8f9b9c5babc1dbc55ce313
Sergio Pedri <sergio0694@live.com>
2019-10-19 16:16:38 +0200

    Added initial benchmark project with Newtonsoft

---
+++ samples/BinaryPack.Benchmark/Implementations/SerializationBenchmark.cs
@@ -0,0 +1,63 @@
+﻿using System.IO;
+using BenchmarkDotNet.Attributes;
+using BinaryPack.Models.Interfaces;
+using Newtonsoft.Json;
+
+namespace BinaryPack.Benchmark.Implementations
+{
+    /// <summary>
+    /// A benchmark for a generic type using different serialization libraries
+    /// </summary>
+    /// <typeparam name="T">The type of model to serialize</typeparam>
+    [MemoryDiagnoser]
+    public class SerializationBenchmark<T> where T : class, IInitializable, new()
+    {
+        // Number of iterations to run
+        private const int N = 1000;
+
+        private readonly T Model = new T();
+
+        /// <summary>
+        /// Initial setup for a benchmarking session
+        /// </summary>
+        [GlobalSetup]
+        public void Setup()
+        {
+            Model.Initialize();
+
+            JsonConvert.SerializeObject(Model);
+            BinaryConverter.Serialize(Model);
+        }
+
+        /// <summary>
+        /// Benchmark run powered by <see cref="JsonSerializer"/>
+        /// </summary>
+        [Benchmark(Baseline = true)]
+        public void NewtonsoftJson()
+        {
+            for (int i = 0; i < N; i++)
+            {
+                using Stream stream = new MemoryStream();
+                using StreamWriter textWriter = new StreamWriter(stream);
+                using JsonTextWriter jsonWriter = new JsonTextWriter(textWriter);
+
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(jsonWriter, Model);
+                jsonWriter.Flush();
+            }
+        }
+
+        /// <summary>
+        /// Benchmark run powered by <see cref="BinaryConverter"/>
+        /// </summary>
+        [Benchmark]
+        public void BinaryPack()
+        {
+            for (int i = 0; i < N; i++)
+            {
+                using Stream stream = new MemoryStream();
+                BinaryConverter.Serialize(Model, stream);
+            }
+        }
+    }
+}
---
+++ samples/BinaryPack.Benchmark/Program.cs
@@ -0,0 +1,20 @@
+﻿using BenchmarkDotNet.Running;
+using BinaryPack.Benchmark.Implementations;
+using BinaryPack.Models;
+
+namespace BinaryPack.Benchmark
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            /* =================
+             * BenchmarkDotNet
+             * ================
+             * In order to run this benchmark, compile this project in Release mode,
+             * then go to its bin\Release\netcoreapp3.0 folder, open a cmd prompt
+             * and type "dotnet BinaryPack.Benchmark.dll */
+            BenchmarkRunner.Run<SerializationBenchmark<UnmanagedArrayModel>>();
+        }
+    }
+}
