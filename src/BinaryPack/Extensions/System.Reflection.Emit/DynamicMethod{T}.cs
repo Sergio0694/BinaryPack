6b12d5ea23976a66ab9083f0f289a450faf93897
Sergio Pedri <sergio0694@live.com>
2019-10-19 10:47:26 +0200

    Added new APIs to the DynamicMethod<T> class

--- src/BinaryPack/Extensions/System.Reflection.Emit/DynamicMethod{T}.cs
+++ src/BinaryPack/Extensions/System.Reflection.Emit/DynamicMethod{T}.cs
@@ -11,7 +11,7 @@ namespace BinaryPack.Extensions.System.Reflection.Emit
     /// A <see langword="class"/> that can be used to easily (lol) create dynamic methods
     /// </summary>
     /// <typeparam name="T">The type of <see cref="Delegate"/> that will be used to wrap the new methods</typeparam>
-    internal static class DynamicMethod<T> where T : Delegate
+    internal sealed class DynamicMethod<T> where T : Delegate
     {
         /// <summary>
         /// The owner type for new dynamic methods
@@ -39,17 +39,39 @@ namespace BinaryPack.Extensions.System.Reflection.Emit
         }
 
         /// <summary>
+        /// Creates a new <see cref="DynamicMethod{T}"/> instance wrapping a given <see cref="DynamicMethod"/>
+        /// </summary>
+        /// <param name="dynamicMethod">The new <see cref="DynamicMethod"/> instance currently being built</param>
+        private DynamicMethod(DynamicMethod dynamicMethod) => MethodInfo = dynamicMethod;
+
+        /// <summary>
+        /// Gets the <see cref="MethodInfo"/> instance representing the new dynamic method being built
+        /// </summary>
+        public MethodInfo MethodInfo { get; }
+
+        /// <summary>
         /// Local counter for dynamic methods of type <typeparamref name="T"/> ever generated
         /// </summary>
         private static int _Count;
 
         /// <summary>
-        /// Creates a new <typeparamref name="T"/> <see langword="delegate"/> for the target owner
+        /// Creates a new unique id for a dynamic method
         /// </summary>
-        /// <param name="builder">An <see cref="Action"/> that builds the IL bytecode for the new method</param>
-        /// <returns>A new dynamic method wrapped as a <typeparamref name="T"/> <see langword="delegate"/></returns>
         [Pure]
-        public static T New(Action<ILGenerator> builder) => New((il, _) => builder(il));
+        private static string GetNewId() => $"__IL__{typeof(T).Name}_{Interlocked.Increment(ref _Count)}";
+
+        /// <summary>
+        /// Creates a new <see cref="DynamicMethod{T}"/> instance with an empty method
+        /// </summary>
+        /// <returns>A <see cref="DynamicMethod{T}"/> instance wrapping an empty <see cref="DynamicMethod"/></returns>
+        [Pure]
+        public static DynamicMethod<T> New()
+        {
+            // Create a new dynamic method
+            DynamicMethod method = new DynamicMethod(GetNewId(), ReturnType, ParameterTypes, OwnerType);
+
+            return new DynamicMethod<T>(method);
+        }
 
         /// <summary>
         /// Creates a new <typeparamref name="T"/> <see langword="delegate"/> for the target owner
@@ -57,13 +79,34 @@ namespace BinaryPack.Extensions.System.Reflection.Emit
         /// <param name="builder">An <see cref="Action"/> that builds the IL bytecode for the new method</param>
         /// <returns>A new dynamic method wrapped as a <typeparamref name="T"/> <see langword="delegate"/></returns>
         [Pure]
-        public static T New(Action<ILGenerator, MethodInfo> builder)
-        {
-            // Create a new dynamic method with a unique id
-            string id = $"__IL__{typeof(T).Name}_{Interlocked.Increment(ref _Count)}";
-            DynamicMethod method = new DynamicMethod(id, ReturnType, ParameterTypes, OwnerType);
+        public static T New(Action<ILGenerator> builder) => New().Build(builder);
 
-            // Build the IL bytecode
+        /// <summary>
+        /// Creates a new <typeparamref name="T"/> <see langword="delegate"/> for the target owner
+        /// </summary>
+        /// <param name="builder">An <see cref="Action"/> that builds the IL bytecode for the new method</param>
+        /// <returns>A new dynamic method wrapped as a <typeparamref name="T"/> <see langword="delegate"/></returns>
+        [Pure]
+        public static T New(Action<ILGenerator, MethodInfo> builder) => New().Build(builder);
+
+        /// <summary>
+        /// Creates a new <typeparamref name="T"/> <see langword="delegate"/> for the current <see cref="MethodInfo"/> handle
+        /// </summary>
+        /// <param name="builder">An <see cref="Action"/> that builds the IL bytecode for the new method</param>
+        /// <returns>A new dynamic method wrapped as a <typeparamref name="T"/> <see langword="delegate"/></returns>
+        [Pure]
+        public T Build(Action<ILGenerator> builder) => Build((il, _) => builder(il));
+
+        /// <summary>
+        /// Creates a new <typeparamref name="T"/> <see langword="delegate"/> for the current <see cref="MethodInfo"/> handle
+        /// </summary>
+        /// <param name="builder">An <see cref="Action"/> that builds the IL bytecode for the new method</param>
+        /// <returns>A new dynamic method wrapped as a <typeparamref name="T"/> <see langword="delegate"/></returns>
+        [Pure]
+        public T Build(Action<ILGenerator, MethodInfo> builder)
+        {
+            // Create and build the new method
+            DynamicMethod method = (DynamicMethod)MethodInfo;
             ILGenerator il = method.GetILGenerator();
             builder(il, method);
 
