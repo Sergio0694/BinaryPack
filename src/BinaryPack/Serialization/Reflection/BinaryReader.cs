7e5ebba5499db511b9f4462c9bc21ddfeb795974
Sergio Pedri <sergio0694@live.com>
2019-10-22 22:35:40 +0200

    Added BinaryReader known members

---
+++ src/BinaryPack/Serialization/Reflection/BinaryReader.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Reflection;
+
+namespace BinaryPack.Serialization.Reflection
+{
+    internal static partial class KnownMembers
+    {
+        /// <summary>
+        /// A <see langword="class"/> containing methods from the <see cref="Buffers.BinaryReader"/> type
+        /// </summary>
+        public static class BinaryReader
+        {
+            /// <summary>
+            /// The <see cref="MethodInfo"/> instance mapping the <see cref="Buffers.BinaryReader.Read{T}()"/> method
+            /// </summary>
+            private static readonly MethodInfo _WriteT = (
+                from method in typeof(Buffers.BinaryReader).GetMethods()
+                where method.Name.Equals(nameof(Buffers.BinaryReader.Read))
+                let parameters = method.GetParameters()
+                let generics = method.GetGenericArguments()
+                where parameters.Length == 0 &&
+                      generics.Length == 1
+                select method).First();
+
+            /// <summary>
+            /// Gets a generic <see cref="MethodInfo"/> instance mapping the <see cref="Buffers.BinaryReader.Read{T}()"/> method
+            /// </summary>
+            [Pure]
+            public static MethodInfo ReadT(Type type) => _WriteT.MakeGenericMethod(type);
+
+            /// <summary>
+            /// The <see cref="MethodInfo"/> instance mapping the <see cref="Buffers.BinaryReader.Read{T}(Span{T})"/> method
+            /// </summary>
+            private static readonly MethodInfo _WriteSpanT = (
+                from method in typeof(Buffers.BinaryReader).GetMethods()
+                where method.Name.Equals(nameof(Buffers.BinaryReader.Read))
+                let parameters = method.GetParameters()
+                let generics = method.GetGenericArguments()
+                where parameters.Length == 1 &&
+                      generics.Length == 1 &&
+                      parameters[0].ParameterType == typeof(Span<>).MakeGenericType(generics[0])
+                select method).First();
+
+            /// <summary>
+            /// Gets a generic <see cref="MethodInfo"/> instance mapping the <see cref="Buffers.BinaryReader.Read{T}(Span{T})"/> method
+            /// </summary>
+            [Pure]
+            public static MethodInfo ReadSpanT(Type type) => _WriteSpanT.MakeGenericMethod(type);
+        }
+    }
+}
