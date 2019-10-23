25859132d43488a255d8c00c26f94fef713d448c
Sergio Pedri <sergio0694@live.com>
2019-10-23 23:42:46 +0200

    Added NullableProcessor<T> and serialization code

---
+++ src/BinaryPack/Serialization/Constants/NullableProcessor{T}.cs
@@ -0,0 +1,54 @@
+﻿using BinaryPack.Attributes;
+
+namespace BinaryPack.Serialization.Processors
+{
+    internal sealed partial class NullableProcessor<T>
+    {
+        /// <summary>
+        /// A <see langword="class"/> that exposes hardcoded indices for local variables for <see cref="NullableProcessor{T}"/>
+        /// </summary>
+        private static class Locals
+        {
+            /// <summary>
+            /// An <see langword="enum"/> with a collection of local variables used during serialization
+            /// </summary>
+            public enum Write
+            {
+                /// <summary>
+                /// The <see cref="bool"/> local variable that indicates whether the input <see cref="System.Nullable{T}"/> instance has a value
+                /// </summary>
+                [LocalType(typeof(bool))]
+                HasValue
+            }
+
+            /// <summary>
+            /// An <see langword="enum"/> with a collection of local variables used during deserialization
+            /// </summary>
+            public enum Read
+            {
+                /// <summary>
+                /// The target <typeparamref name="T"/> array
+                /// </summary>
+                ArrayT,
+
+                /// <summary>
+                /// The <see cref="int"/> local variable to track the length of the target <typeparamref name="T"/> array
+                /// </summary>
+                [LocalType(typeof(int))]
+                Length,
+
+                /// <summary>
+                /// The <see cref="int"/> local variable for the loop counter
+                /// </summary>
+                [LocalType(typeof(int))]
+                I,
+
+                /// <summary>
+                /// The <see langword="ref"/> <typeparamref name="T"/> variable, used to iterate arrays of reference types
+                /// </summary>
+                RefT
+            }
+        }
+    }
+}
+
---
+++ src/BinaryPack/Serialization/Processors/NullableProcessor{T}.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+using BinaryPack.Serialization.Constants;
+using BinaryPack.Serialization.Processors.Abstract;
+using BinaryPack.Serialization.Reflection;
+
+namespace BinaryPack.Serialization.Processors
+{
+    /// <summary>
+    /// A <see langword="class"/> responsible for creating the serializers and deserializers for <see cref="Nullable{T}"/> types
+    /// </summary>
+    internal sealed partial class NullableProcessor<T> : TypeProcessor<T?> where T : struct
+    {
+        /// <summary>
+        /// Gets the singleton <see cref="NullableProcessor{T}"/> instance to use
+        /// </summary>
+        public static NullableProcessor<T> Instance { get; } = new NullableProcessor<T>();
+
+        /// <inheritdoc/>
+        protected override void EmitSerializer(ILGenerator il)
+        {
+            /* This processor has a special path for bool? variables. In order to save one
+             * extra byte written to the stream, it leverages the fact that bool variables
+             * can only have the value of either 0 or 1, and serializes null values as -1.
+             * To do so, the serialized value is being written to the writer as a sbyte. */
+            if (typeof(T) == typeof(bool))
+            {
+                // if (!obj.hasValue) writer.Write<sbyte>(-1);
+                Label
+                    isNotNull = il.DefineLabel(),
+                    write = il.DefineLabel();
+                il.EmitLoadArgument(Arguments.Write.RefBinaryWriter);
+                il.EmitLoadArgument(Arguments.Write.T);
+                il.EmitReadMember(typeof(T?).GetField("hasValue", BindingFlags.NonPublic));
+                il.Emit(OpCodes.Brtrue_S, isNotNull);
+                il.EmitLoadInt32(-1);
+                il.Emit(OpCodes.Br_S, write);
+
+                // else writer.Write<sbyte>(obj.value);
+                il.MarkLabel(isNotNull);
+                il.EmitLoadArgument(Arguments.Write.T);
+                il.EmitReadMember(typeof(T?).GetField("value", BindingFlags.NonPublic));
+                il.MarkLabel(write);
+                il.EmitCall(KnownMembers.BinaryWriter.WriteT(typeof(sbyte)));
+            }
+            else
+            {
+                /* For all other struct types, first a bool is written to the stream to
+                 * indicate whether the input value is null or not. If it is null, the
+                 * rest of the serialization is skipped. If it is not, there are two cases
+                 * to consider: if T is unmanaged, the whole value is just written directly
+                 * to the writer, otherwise the right ObjectProcessor<T> instanced is invoked. */
+                il.DeclareLocals<Locals.Write>();
+
+                // writer.Write(hasValue = obj.hasValue);
+                il.EmitLoadArgument(Arguments.Write.RefBinaryWriter);
+                il.EmitLoadArgument(Arguments.Write.T);
+                il.EmitReadMember(typeof(T?).GetField("hasValue", BindingFlags.NonPublic));
+                il.Emit(OpCodes.Dup);
+                il.EmitStoreLocal(Locals.Write.HasValue);
+                il.EmitCall(KnownMembers.BinaryWriter.WriteT(typeof(bool)));
+
+                // if (hasValue) { }
+                Label isNull = il.DefineLabel();
+                il.EmitLoadArgument(Locals.Write.HasValue);
+                il.Emit(OpCodes.Brfalse_S, isNull);
+
+                if (typeof(T).IsUnmanaged())
+                {
+                    // writer.Write(obj.value);
+                    il.EmitLoadArgument(Arguments.Write.RefBinaryWriter);
+                    il.EmitLoadArgument(Arguments.Write.T);
+                    il.EmitReadMember(typeof(T?).GetField("value", BindingFlags.NonPublic));
+                    il.EmitCall(KnownMembers.BinaryWriter.WriteT(typeof(T)));
+                }
+                else
+                {
+                    // ObjectProcessor<T>.Serialize(obj.value, ref writer);
+                    il.EmitLoadArgument(Arguments.Write.T);
+                    il.EmitReadMember(typeof(T?).GetField("value", BindingFlags.NonPublic));
+                    il.EmitLoadArgument(Arguments.Write.RefBinaryWriter);
+                    il.EmitCall(KnownMembers.TypeProcessor.SerializerInfo(typeof(ObjectProcessor<>), typeof(T)));
+                }
+
+                il.MarkLabel(isNull);
+            }
+
+            // return;
+            il.Emit(OpCodes.Ret);
+        }
+
+        /// <inheritdoc/>
+        protected override void EmitDeserializer(ILGenerator il)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
