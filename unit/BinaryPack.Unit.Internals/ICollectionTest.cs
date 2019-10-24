7e5c89ff8d90fe6ff05456145fe5af0809a01d9c
Sergio Pedri <sergio0694@live.com>
2019-10-24 22:08:03 +0200

    Added ICollection<T> tests

--- unit/BinaryPack.Unit.Internals/ICollectionTest.cs
+++ unit/BinaryPack.Unit.Internals/ICollectionTest.cs
@@ -12,24 +12,24 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace BinaryPack.Unit.Internals
 {
     [TestClass]
-    public class IEnumerableTest
+    public class ICollectionTest
     {
-        // Test method for a generic sequences of reference types
-        public static void Test<T>(IEnumerable<T>? sequence) where T : class, IEquatable<T>
+        // Test method for a generic collection of reference types
+        public static void Test<T>(ICollection<T>? sequence) where T : class, IEquatable<T>
         {
             // Serialization
             BinaryWriter writer = new BinaryWriter(BinaryWriter.DefaultSize);
-            IEnumerableProcessor<T>.Instance.Serializer(sequence, ref writer);
+            ICollectionProcessor<T>.Instance.Serializer(sequence, ref writer);
             Span<byte> span = MemoryMarshal.CreateSpan(ref Unsafe.AsRef(writer.Span.GetPinnableReference()), writer.Span.Length);
             BinaryReader reader = new BinaryReader(span);
-            IEnumerable<T>? result = IEnumerableProcessor<T>.Instance.Deserializer(ref reader);
+            ICollection<T>? result = ICollectionProcessor<T>.Instance.Deserializer(ref reader);
 
             // Equality check
             if (sequence == null) Assert.IsNull(result);
             else
             {
                 Assert.IsNotNull(result);
-                Assert.IsTrue(sequence.Count() == result!.Count());
+                Assert.IsTrue(sequence.Count == result!.Count);
                 Assert.IsTrue(sequence.Zip(result).All(p =>
                 {
                     if (p.First == null && p.Second == null) return true;
@@ -39,80 +39,80 @@ namespace BinaryPack.Unit.Internals
         }
 
         [TestMethod]
-        public void ReferenceTypeNullIEnumerableSerializationTest() => Test(default(IEnumerable<MessagePackSampleModel>));
+        public void ReferenceTypeNullICollectionSerializationTest() => Test(default(ICollection<MessagePackSampleModel>));
 
         [TestMethod]
-        public void ReferenceTypeEmptyIEnumerableSerializationTest() => Test(Array.Empty<MessagePackSampleModel>());
+        public void ReferenceTypeEmptyICollectionSerializationTest() => Test(Array.Empty<MessagePackSampleModel>());
 
         [TestMethod]
-        public void ReferenceTypeIEnumerableSerializationTest1() => Test(new[] { new MessagePackSampleModel { Compact = true, Schema = 17 } });
+        public void ReferenceTypeICollectionSerializationTest1() => Test(new[] { new MessagePackSampleModel { Compact = true, Schema = 17 } });
 
         [TestMethod]
-        public void ReferenceTypeIEnumerableSerializationTest2() => Test((
+        public void ReferenceTypeICollectionSerializationTest2() => Test((
             from i in Enumerable.Range(0, 10)
             let compact = i % 2 == 0
             let model = new MessagePackSampleModel { Compact = compact, Schema = i }
             select model).ToArray());
 
         [TestMethod]
-        public void ReferenceTypeIEnumerableSerializationTest3() => Test((
+        public void ReferenceTypeICollectionSerializationTest3() => Test((
             from i in Enumerable.Range(0, 10)
             let compact = i % 2 == 0
             let model = compact ? null : new MessagePackSampleModel { Compact = compact, Schema = i }
             select model).ToArray());
 
         [TestMethod]
-        public void StringNullIEnumerableSerializationTest() => Test(default(IEnumerable<string>));
+        public void StringNullICollectionSerializationTest() => Test(default(ICollection<string>));
 
         [TestMethod]
-        public void StringEmptyIEnumerableSerializationTest() => Test(Array.Empty<string>());
+        public void StringEmptyICollectionSerializationTest() => Test(Array.Empty<string>());
 
         [TestMethod]
-        public void StringIEnumerableSerializationTest1() => Test(new[] { RandomProvider.NextString(60) });
+        public void StringICollectionSerializationTest1() => Test(new[] { RandomProvider.NextString(60) });
 
         [TestMethod]
-        public void StringIEnumerableSerializationTest2() => Test((
+        public void StringICollectionSerializationTest2() => Test((
             from _ in Enumerable.Range(0, 10)
             select RandomProvider.NextString(60)).ToArray());
 
         [TestMethod]
-        public void StringIEnumerableSerializationTest3() => Test((
+        public void StringICollectionSerializationTest3() => Test((
             from i in Enumerable.Range(0, 10)
             let isNull = i % 2 == 0
             let text = isNull ? null : RandomProvider.NextString(60)
             select text).ToArray());
 
-        // Test method for sequences of an unmanaged type
-        public static void Test(IEnumerable<DateTime>? sequence)
+        // Test method for collections of an unmanaged type
+        public static void Test(ICollection<DateTime>? sequence)
         {
             // Serialization
             BinaryWriter writer = new BinaryWriter(BinaryWriter.DefaultSize);
-            IEnumerableProcessor<DateTime>.Instance.Serializer(sequence, ref writer);
+            ICollectionProcessor<DateTime>.Instance.Serializer(sequence, ref writer);
             Span<byte> span = MemoryMarshal.CreateSpan(ref Unsafe.AsRef(writer.Span.GetPinnableReference()), writer.Span.Length);
             BinaryReader reader = new BinaryReader(span);
-            IEnumerable<DateTime>? result = IEnumerableProcessor<DateTime>.Instance.Deserializer(ref reader);
+            ICollection<DateTime>? result = ICollectionProcessor<DateTime>.Instance.Deserializer(ref reader);
 
             // Equality check
             if (sequence == null) Assert.IsNull(result);
             else
             {
                 Assert.IsNotNull(result);
-                Assert.AreEqual(sequence.Count(), result!.Count());
+                Assert.AreEqual(sequence.Count, result!.Count);
                 Assert.IsTrue(MemoryMarshal.AsBytes(sequence.ToArray().AsSpan()).SequenceEqual(MemoryMarshal.AsBytes(result.ToArray().AsSpan())));
             }
         }
 
         [TestMethod]
-        public void UnmanagedTypeNullIEnumerableSerializationTest() => Test(default);
+        public void UnmanagedTypeNullICollectionSerializationTest() => Test(default);
 
         [TestMethod]
-        public void UnmanagedTypeEmptyIEnumerableSerializationTest() => Test(Array.Empty<DateTime>());
+        public void UnmanagedTypeEmptyICollectionSerializationTest() => Test(Array.Empty<DateTime>());
 
         [TestMethod]
-        public void UnmanagedTypeIEnumerableSerializationTest1() => Test(new[] { RandomProvider.NextDateTime() });
+        public void UnmanagedTypeICollectionSerializationTest1() => Test(new[] { RandomProvider.NextDateTime() });
 
         [TestMethod]
-        public void UnmanagedTypeIEnumerableSerializationTest2() => Test((
+        public void UnmanagedTypeICollectionSerializationTest2() => Test((
             from i in Enumerable.Range(0, 10)
             select RandomProvider.NextDateTime()).ToArray());
     }
