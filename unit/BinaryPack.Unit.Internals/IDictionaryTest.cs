67d5e40755e4eeff8f70d0dfb6c902810726eb0b
Sergio Pedri <sergio0694@live.com>
2019-10-26 01:56:17 +0200

    Added IDictionaryProcessor<K, V> tests

--- unit/BinaryPack.Unit.Internals/IDictionaryTest.cs
+++ unit/BinaryPack.Unit.Internals/IDictionaryTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -12,26 +13,26 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace BinaryPack.Unit.Internals
 {
     [TestClass]
-    public class DictionaryTest
+    public class IDictionaryTest
     {
         // Test method for a generic dictionary
-        public static void Test<K, V>(Dictionary<K, V?>? dictionary)
+        public static void Test<K, V>(IDictionary<K, V?>? dictionary)
             where K : IEquatable<K>
             where V : class, IEquatable<V>
         {
             // Serialization
             BinaryWriter writer = new BinaryWriter(BinaryWriter.DefaultSize);
-            DictionaryProcessor<K, V?>.Instance.Serializer(dictionary, ref writer);
+            IDictionaryProcessor<K, V?>.Instance.Serializer(dictionary, ref writer);
             Span<byte> span = MemoryMarshal.CreateSpan(ref Unsafe.AsRef(writer.Span.GetPinnableReference()), writer.Span.Length);
             BinaryReader reader = new BinaryReader(span);
-            Dictionary<K, V?>? result = DictionaryProcessor<K, V?>.Instance.Deserializer(ref reader);
+            IDictionary<K, V?>? result = IDictionaryProcessor<K, V?>.Instance.Deserializer(ref reader);
 
             // Equality check
             Assert.IsTrue(StructuralComparer.IsMatch(dictionary, result));
         }
 
         [TestMethod]
-        public void IntAndReferenceTypeNullDictionarySerializationTest() => Test(default(Dictionary<int, MessagePackSampleModel?>));
+        public void IntAndReferenceTypeNullDictionarySerializationTest() => Test(default(IDictionary<int, MessagePackSampleModel?>));
 
         [TestMethod]
         public void IntAndReferenceTypeEmptyDictionarySerializationTest() => Test(new Dictionary<int, MessagePackSampleModel?>());
@@ -40,10 +41,26 @@ namespace BinaryPack.Unit.Internals
         public void IntAndReferenceTypeDictionarySerializationTest1() => Test(new Dictionary<int, MessagePackSampleModel?> { [17] = new MessagePackSampleModel { Compact = true, Schema = 127 } });
 
         [TestMethod]
-        public void ReferenceTypeDictionarySerializationTest2() => Test((
+        public void ReferenceTypeIDictionarySerializationTest2() => Test((
             from i in Enumerable.Range(0, 10)
             let compact = i % 2 == 0
             let model = new MessagePackSampleModel { Compact = compact, Schema = i }
             select (i, model)).ToDictionary<(int Key, MessagePackSampleModel Value), int, MessagePackSampleModel?>(p => p.Key, p => p.Value));
+
+        [TestMethod]
+        public void ReferenceTypeIDictionarySerializationTest3()
+        {
+            ConcurrentDictionary<int, MessagePackSampleModel?> dictionary = new ConcurrentDictionary<int, MessagePackSampleModel?>();
+            foreach ((int key, MessagePackSampleModel? value) in
+                from i in Enumerable.Range(0, 10)
+                let compact = i % 2 == 0
+                let model = new MessagePackSampleModel { Compact = compact, Schema = i }
+                select (i, model))
+            {
+                dictionary.TryAdd(key, value);
+            }
+
+            Test(dictionary);
+        }
     }
 }
