48b23a0f6269b10b560e821320291fcdae84fe60
Sergio Pedri <sergio0694@live.com>
2019-10-25 12:22:56 +0200

    Added serialization attributes

---
+++ src/BinaryPack/Attributes/BinarySerializationAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using BinaryPack.Enums;
+
+namespace BinaryPack.Attributes
+{
+    /// <summary>
+    /// A custom <see cref="Attribute"/> that marks an object that supports serialization
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+    public sealed class BinarySerializationAttribute : Attribute
+    {
+        /// <summary>
+        /// Gets the serialization mode to use for the target object
+        /// </summary>
+        public SerializationMode Mode { get; }
+
+        /// <summary>
+        /// Creates a new <see cref="BinarySerializationAttribute"/> instance with the specified mode
+        /// </summary>
+        /// <param name="mode">The requested mode to use to serialize the target object</param>
+        public BinarySerializationAttribute(SerializationMode mode) => Mode = mode;
+    }
+}
---
+++ src/BinaryPack/Attributes/IgnoredMemberAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BinaryPack.Attributes
+{
+    /// <summary>
+    /// A custom <see cref="Attribute"/> that marks an member that doesn't support serialization
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public sealed class IgnoredMemberAttribute : Attribute { }
+}
